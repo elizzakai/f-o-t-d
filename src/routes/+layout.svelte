c6f4bc83d3b3b092551bf372b9529123b6bbc458
elizzakai <71152176+elizzakai@users.noreply.github.com>
2025-04-15 00:28:36 -0500

    some updates to the barebones css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <style>
+
 	h1 {
 		font-size: medium;
 		text-align: center;
@@ -14,9 +15,29 @@
 		color: rgb(72, 106, 203);
 		font-family: sans-serif;
 	}
+	
+	.blurb {
+		width: 742px;
+		height: 450px;
+		width: 50%;       /* or any fixed width */
+		margin: auto;     /* centers horizontally */
+		box-sizing: border-box;
+		flex-shrink: 0;
+		border-radius: 31px;
+		background-color: #F5D09B;
+		
+	}
+	
+	:global(body) {
+		background-color: rgba(255, 238, 214, 1);
+	}
 </style>
 
-<h1>hello there, my man you are the base bones! 
-im boned down</h1>
+
+<container class="parent">parent contains everything
+<h1>hello there, my man you are the base bones! </h1>
+<div class="blurb">
+</div>
+</container>
 
 {@render children()}
