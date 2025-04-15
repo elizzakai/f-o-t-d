c09a0527301958fb9dd4d864a5b761e1054b7fd2
elizzakai <71152176+elizzakai@users.noreply.github.com>
2025-04-14 23:25:47 -0500

    clown behavior

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,19 @@
 	let { children } = $props();
 </script>
 
+<style>
+	h1 {
+		font-size: medium;
+		text-align: center;
+	}
+	
+	h1:hover {
+		color: rgb(72, 106, 203);
+		font-family: sans-serif;
+	}
+</style>
+
+<h1>hello there, my man you are the base bones! 
+im boned down</h1>
+
 {@render children()}
